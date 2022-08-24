65e06c5b87e6745fe61252f75b94692844a0507a
dmitry.kondar <dmitry.kondar@speedandfunction.com>
2022-08-24 14:30:10 +0300

    comment out calls to old api

--- project/js/main.js
+++ project/js/main.js
@@ -7,23 +7,23 @@ function showPicture(){
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
 }
 
-function getweather(searchQurey) {
-  var url = "https://api.openwethermap.ogr/data/2.5/weather?q="+searchQurey+"&units=imperial&APPID="+apiKey;
+// function getweather(searchQurey) {
+//   var url = "https://api.openwethermap.ogr/data/2.5/weather?q="+searchQurey+"&units=imperial&APPID="+apiKey;
 
-  $(".city").text("");
-  $(".temp").text("");
-  $(".error-message").text("");
+//   $(".city").text("");
+//   $(".temp").text("");
+//   $(".error-message").text("");
 
-  $.ajax(url, {success: function(data) {
-    console.log(data);
-    $(".city").text(data.name);
-    $(".temp").text(data.main.temp);
-  }, error: function(error) {
-    $(".error-message").text("An error occured!");
-  }})
-}
+//   $.ajax(url, {success: function(data) {
+//     console.log(data);
+//     $(".city").text(data.name);
+//     $(".temp").text(data.main.temp);
+//   }, error: function(error) {
+//     $(".error-message").text("An error occured!");
+//   }})
+// }
 
-function searchWeather() {
-  var searchQurey = $(".search").val();
-  getweather(searchQurey);
-}
+// function searchWeather() {
+//   var searchQurey = $(".search").val();
+//   getweather(searchQurey);
+// }
