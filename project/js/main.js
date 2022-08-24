b1fb0d98b89b36f0b6f7d0b8b4296aeba4315eb9
dmitry.kondar <dmitry.kondar@speedandfunction.com>
2022-08-24 13:50:44 +0300

    add dynamic query

--- project/js/main.js
+++ project/js/main.js
@@ -1,22 +1,29 @@
 function showPicture(){
-  $(document).ready(function(){
-    getweather();
-  })
-  function getweather() {
-    var url = "https://api.openwethermap.ogr/data/2.5/weather?q=Boston&units=imperial&APPID="+apiKey;
-
-    $.ajax(url, {success: function(data) {
-      console.log(data);
-      $(".city").text(data.name);
-      $(".temp").text(data.main.temp);
-    }})
-  }
-
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
   $("p").html("High five! You're building your first web app!");
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+}
+
+function getweather(searchQurey) {
+  var url = "https://api.openwethermap.ogr/data/2.5/weather?q="+searchQurey+"&units=imperial&APPID="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
+
+  $.ajax(url, {success: function(data) {
+    console.log(data);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error) {
+    $(".error-message").text("An error occured!");
+  }})
+}
+
+function searchWeather() {
+  var searchQurey = $(".search").val();
+  getweather(searchQurey);
 }
