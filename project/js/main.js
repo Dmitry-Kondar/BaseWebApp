631b2f9b9e94f7203670cf4ab51a230433427a9e
dmitry.kondar <dmitry.kondar@speedandfunction.com>
2022-08-24 13:14:59 +0300

    add api request (no credentials)

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,17 @@
 function showPicture(){
+  $(document).ready(function(){
+    getweather();
+  })
+  function getweather() {
+    var url = "https://api.openwethermap.ogr/data/2.5/weather?q=Boston&units=imperial&APPID="+apiKey;
+
+    $.ajax(url, {success: function(data) {
+      console.log(data);
+      $(".city").text(data.name);
+      $(".temp").text(data.main.temp);
+    }})
+  }
+
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
